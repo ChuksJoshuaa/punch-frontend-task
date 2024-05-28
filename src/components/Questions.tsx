c9aaa8b801ac035c88b62538246b0f2e1e603c2a
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2024-05-28 14:17:53 +0100

    Questions UI completed

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -1,28 +1,76 @@
-import { questionsData } from "../utils/header";
 import { useState } from "react";
+import { questionsData } from "../utils/header";
 
 const Questions = () => {
   const [selectedIndex, setSelectedIndex] = useState(2);
   return (
-    <div className="relative pt-10 bg-white">
+    <div className="relative pt-3 bg-[#F3F3F3]">
       <div
         className="absolute top-0 left-0 w-full h-24 bg-blue-500 transform -skew-y-2 origin-top-left"
-        style={{ zIndex: 10, backgroundColor: "white" }}
+        style={{ zIndex: 10, backgroundColor: "#F3F3F3" }}
       ></div>
 
       <div className="relative z-10 flex flex-col items-center justify-center py-16 mt-20">
-        <div className="text-center mt-6 font-bold text-2xl md:text-3xl lg:text-4xl leading-tight">
+        <div className="text-center font-bold text-[22px] md:text-[40px] lg:text-[54px] leading-tight mb-20">
           Frequently asked questions
         </div>
 
         {questionsData.map((item, idx) => (
-          <div key={`${item.id}-${idx}`} className="flex flex-col">
-            <div onClick={() => setSelectedIndex(idx)}>
-              {item.isGeneral && <div>General</div>}
-              {item.isProcess && <div>Process</div>}
-              <div>
-                <div>
-                  <h3>{item.name}</h3>
+          <div
+            key={`${item.id}-${idx}`}
+            className="flex flex-col relative mx-auto w-full z-10 flex flex-col md:flex-row items-center justify-center"
+          >
+            <div
+              onClick={() => setSelectedIndex(idx)}
+              className={`w-full flex flex-row flex-wrap justify-between border-b-1 border-[#D6D6D6] ${
+                idx === 0 && selectedIndex === idx
+                  ? "border-t-1 border-[#D6D6D6] bg-[#E8E8E8]"
+                  : selectedIndex === idx
+                  ? "bg-[#E8E8E8]"
+                  : idx === 0
+                  ? "border-t-1 border-[#D6D6D6]"
+                  : ""
+              }`}
+            >
+              {item.isGeneral ? (
+                <div
+                  className={`md:w-[15%] px-[10px] md:px-[20px] md:border-r-1 md:border-[#D6D6D6] py-[14px] md:py-[23px]`}
+                >
+                  <h2
+                    className={`text-lg md:text-xl font-semibold leading-normal md:leading-[24px] text-center text-[#202229]`}
+                  >
+                    General
+                  </h2>
+                </div>
+              ) : (
+                <div className="md:w-[15%] pr-[20px] py-[14px] md:py-[23px] hidden md:block">
+                  <h2 className="text-lg md:text-xl font-semibold leading-normal md:leading-[15%]px] text-[#F3F3F3] text-[#202229]">
+                    .
+                  </h2>
+                </div>
+              )}
+              {item.isProcess ? (
+                <div className="md:w-[15%] px-[10px] md:px-[20px] py-[14px] md:py-[23px]">
+                  <h2 className="text-lg md:text-xl font-semibold leading-normal md:leading-[24px] text-[#202229] text-center whitespace-nowrap">
+                    Joining Process
+                  </h2>
+                </div>
+              ) : (
+                <div className="md:w-[15%] py-[14px] md:py-[23px] hidden md:block">
+                  <h2 className="text-lg md:text-xl font-semibold leading-normal md:leading-[24px] text-[#F3F3F3] text-[#202229]">
+                    .
+                  </h2>
+                </div>
+              )}
+              <div className={`md:w-[60%] md:pl-[3em] ${item.isProcess ? 'md:border-l-1 md:border-[#D6D6D6]': ''} `}>
+                <div className="flex justify-start md:justify-between items-right md:items-center pt-6 px-5 md:px-[3em] pb-3">
+                  <h3
+                    className={`text-sm leading-normal text-[#202229] mx-2 ${
+                      selectedIndex === idx ? "font-semibold" : "font-normal"
+                    }`}
+                  >
+                    {item.name}
+                  </h3>
                   {selectedIndex === idx && (
                     <svg
                       width="27"
